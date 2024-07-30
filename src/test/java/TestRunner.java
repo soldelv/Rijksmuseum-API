391dbb7e00b9f95566bed58c49705bcd652c05b7
soldelv <jsoldelvalle@gmail.com>
2024-07-30 01:08:34 +0100

    updated report file

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,16 +1,20 @@
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
         glue = "stepsdefinitions",
-        plugin = {"pretty",
-                "json:target/cucumber-reports/cucumber.json",
-                "html:target/cucumber-reports/cucumber.html"},
+        plugin = {"pretty", "json:target/cucumber-reports/cucumber.json"},
         monochrome = true,
         tags = ("@regressions"),
         publish = true
@@ -24,5 +28,12 @@ public class TestRunner {
 
     @AfterClass
     public static void finalSequence() {
+        File reportOutputDirectory = new File("target/cucumber-reports");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("target/cucumber-reports/cucumber.json");
+
+        Configuration configuration = new Configuration(reportOutputDirectory, "Rijksmuseum API");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        reportBuilder.generateReports();
     }
 }
